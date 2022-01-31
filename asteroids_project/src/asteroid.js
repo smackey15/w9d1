3e8e670ccbec68a5edd668967d4d2f490e402416
Katie Hang <katiehang1991@gmail.com>
2022-01-31 17:28:32 -0500

    last commit

--- asteroids_project/src/asteroid.js
+++ asteroids_project/src/asteroid.js
@@ -1,8 +1,9 @@
+const MovingObject = require("./moving_object.js");
 const Util = require("./util.js");
 
 function Asteroid(object) {
     this.color = "purple";
     this.radius = 10;
     this.pos = object.pos;
-    
+    MovingObject.call(object, object.pos);
 }
